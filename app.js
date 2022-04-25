a3d88ca7f6d230d09aad308a302ff0ad7b4c0f4e
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-04-25 18:28:21 +0100

    added nested options for mentorship program selection
    Signed-off-by: Precious Abubakar <preciousdanabubakar@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 // Initializes your app with your bot token and signing secret
@@ -14,67 +14,152 @@ const app = new App({
 
 // When a user joins the team, the bot sends a DM to the newcommer asking them how they would like to contribute
 
-app.event('team_join', async ({ event, client, logger }) => {
+app.event("team_joined", async ({ event, client, logger }) => {
   try {
     const result = await client.chat.postMessage({
       channel: event.user.id,
       blocks: [
         {
-          type: 'section',
+          type: "section",
           text: {
-            type: 'mrkdwn',
+            type: "mrkdwn",
             text: `Welcome to *CHAOSS Community* <@${event.user.id}>! 🎉How would you like to get started? \n\nI want to..`,
           },
         },
         {
-          type: 'section',
+          type: "section",
           text: {
-            type: 'mrkdwn',
-            text: '*Develop Metrics*',
+            type: "mrkdwn",
+            text: "*Develop Metrics*",
           },
           accessory: {
-            type: 'button',
+            type: "button",
             text: {
-              type: 'plain_text',
-              text: 'Choose',
+              type: "plain_text",
+              text: "Choose",
               emoji: true,
             },
 
-            action_id: 'develop',
+            action_id: "develop",
           },
         },
         {
-          type: 'section',
+          type: "section",
           text: {
-            type: 'mrkdwn',
-            text: '*Join Meeting*',
+            type: "mrkdwn",
+            text: "*Join a Meeting*",
           },
           accessory: {
-            type: 'button',
+            type: "button",
             text: {
-              type: 'plain_text',
-              text: 'Choose',
+              type: "plain_text",
+              text: "Choose",
               emoji: true,
             },
 
-            action_id: 'joinMeet',
+            action_id: "joinMeet",
           },
         },
         {
-          type: 'section',
+          type: "section",
           text: {
-            type: 'mrkdwn',
-            text: '*Contribute or Review Code*',
+            type: "mrkdwn",
+            text: "*Contribute or Review Code*",
           },
           accessory: {
-            type: 'button',
+            type: "button",
             text: {
-              type: 'plain_text',
-              text: 'Choose',
+              type: "plain_text",
+              text: "Choose",
               emoji: true,
             },
 
-            action_id: 'contribute',
+            action_id: "contribute",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Help with the Website.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Choose",
+              emoji: true,
+            },
+            value: "website",
+            action_id: "helpWithWebsite",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Write or Edit Documentation.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+              emoji: true,
+            },
+            value: "docs",
+            action_id: "docs",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Contribute through a Mentorship Program.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+              emoji: true,
+            },
+            value: "mentorship",
+            action_id: "mentorship",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Implement Metrics in my Project.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+              emoji: true,
+            },
+            value: "implement_metrics",
+            action_id: "implement_metrics",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Learn About Something Else.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+              emoji: true,
+            },
+            value: "learn_something_else",
+            action_id: "learn_something_else",
           },
         },
       ],
@@ -87,7 +172,7 @@ app.event('team_join', async ({ event, client, logger }) => {
 });
 
 // handle the button click  and shows the responses
-app.action('develop', async ({ body, ack, say }) => {
+app.action("develop", async ({ ack, say }) => {
   await ack();
   await say(
     `There are 5 Working Groups that develop metrics based on different aspects of open source community health: Risk, Value, Evolution, DEI, and Common.  More information about each of these groups can be found here: <https://handbook.chaoss.community/community-handbook/community-initiatives/working-groups> and the metrics are developed during our Working Group meetings.
@@ -95,7 +180,7 @@ app.action('develop', async ({ body, ack, say }) => {
   );
 });
 
-app.action('joinMeet', async ({ body, ack, say }) => {
+app.action("joinMeet", async ({ ack, say }) => {
   // Acknowledge the action
   await ack();
   await say(
@@ -103,12 +188,109 @@ app.action('joinMeet', async ({ body, ack, say }) => {
     `
   );
 });
-app.action('contribute', async ({ body, ack, say }) => {
+app.action("contribute", async ({ ack, say }) => {
   // Acknowledge the action
   await ack();
   await say(
-    `You are welcome to contribute. All CHAOSS meetings are open to everyone, and they happen virtually at  <https://zoom.us/my/chaoss> We recommend a good first meeting is our Weekly Community Call (Every Tuesday at 11:00 am US Central/Chicago time) but you can see a calendar of all our meetings at https://chaoss.community/participate.
+    `You are welcome to contribute. Please see this documentation: https://handbook.chaoss.community/community-handbook/contributing/development.
     `
+  );
+});
+app.action("helpWithWebsite", async ({ ack, say }) => {
+  await ack();
+  await say(
+    `The first step is getting to know our community! You can connect with us in any of the ways described in our Participate page here: https://chaoss.community/participate.
+    `
+  );
+});
+app.action("docs", async ({ ack, say }) => {
+  await ack();
+  await say(
+    `Please see this documentation: https://handbook.chaoss.community/community-handbook/contributing/documentation.
+    `
+  );
+});
+app.action("mentorship", async ({ ack, event }) => {
+  await ack();
+  try {
+    const result = await client.chat.postMessage({
+      channel: event.user.id,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Which mentorship program are you interested in?",
+          },
+          accessory: {
+            type: "radio_buttons",
+            action_id: "mentorship_selection",
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "Outreachy",
+                emoji: true,
+              },
+              value: "Outreachy",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Google Summer of Code",
+                emoji: true,
+              },
+              value: "GSOC",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Google Season of Docs",
+                emoji: true,
+              },
+              value: "GSOD",
+            },
+          ],
+        },
+      ],
+    });
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
+app.action("mentorship_selection", async ({ action, ack, say }) => {
+  await ack();
+  if (action.value === "Outreachy") {
+    await say(
+      `The Outreachy Application period has ended for our participation in this program this year. You can still join the CHAOSS community in any of the ways found on our Participate page here: https://chaoss.community/participate.`
+    );
+  }
+  if (action.value === "GSOC") {
+    await say(
+      `The Google Summer of Code Application period has ended for our participation in this program this year. You can still join the CHAOSS community in any of the ways found on our Participate page here: https://chaoss.community/participate.`
+    );
+  }
+  if(action.value === "GSOD") {
+    await say(
+      `Welcome! If you haven’t yet, please join the <#C03C239HN1F> Slack channel  and take note of the pinned item at the top of the channel for more information about next steps.`
+    );
+  }
+});
+
+app.action("implement_metrics", async ({ ack, say }) => {
+  await ack();
+  await say(
+    `We encourage you to join one of our Working Groups for specific questions about implementing your metrics. You can read more about them here: https://handbook.chaoss.community/community-handbook/community-initiatives/working-groups.`
+  );
+});
+
+app.action("learn_something_else", async ({ ack, say }) => {
+  await ack();
+  await say(
+    `We encourage you to read through our Community Handbook: https://handbook.chaoss.community/community-handbook/, and if you still can’t find what you’re looking for, feel free to ask your question in our #newcomers slack channel.`
   );
 });
 
@@ -116,18 +298,18 @@ app.message(/hello|hey|hi/i, async ({ message, say }) => {
   await say({
     blocks: [
       {
-        type: 'section',
+        type: "section",
         text: {
-          type: 'mrkdwn',
+          type: "mrkdwn",
           text: `Welcome to CHAOSS Community <@${message.user}>!`,
         },
         accessory: {
-          type: 'button',
+          type: "button",
           text: {
-            type: 'plain_text',
-            text: 'Click Me',
+            type: "plain_text",
+            text: "Click Me",
           },
-          action_id: 'button_click',
+          action_id: "button_click",
         },
       },
     ],
@@ -135,18 +317,11 @@ app.message(/hello|hey|hi/i, async ({ message, say }) => {
   });
 });
 
-// handler to respond to click
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
-
 // welcomes a new member to the newcomer channel
-const channelId = 'C03AS6X6RJS';
+const channelId = "C03AS6X6RJS";
 
 // When a user joins the team, send a message in a predefined channel asking them to introduce themselves
-app.event('team_join', async ({ event, client, logger }) => {
+app.event("team_join", async ({ event, client, logger }) => {
   try {
     const result = await client.chat.postMessage({
       channel: channelId,
@@ -162,5 +337,5 @@ app.event('team_join', async ({ event, client, logger }) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
