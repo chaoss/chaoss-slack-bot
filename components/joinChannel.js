4eed45e7970e93f5b2f1b7a2ebefae450c33e070
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-10-02 04:02:32 +0100

    fix: channel-not-found error from teamJoin event handler
    Signed-off-by: Precious Abubakar <preciousdanabubakar@gmail.com>

--- components/joinChannel.js
+++ components/joinChannel.js
@@ -1,24 +1,24 @@
-const testChannel = 'C03D3L8TNMD';
+const testChannel = "C03D3L8TNMD";
 
 async function memberJoin(event, client, logger) {
-  try {
-    console.log(event);
-    return await client.chat.postMessage({
-      channel: testChannel,
+	try {
+		return await client.chat.postMessage({
+			channel: testChannel,
       blocks: [
         {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `Welcome to the team, <@${event.user}>! 🎉.`,
+            text: `Welcome to the team, <@${event.user.id}>! 🎉.`,
           },
         },
       ],
-    });
-  } catch (error) {
-    console.log(error);
-    logger.error(error);
-  }
+			text: `Welcome to the team, <@${event.user.id}>! 🎉.`,
+		});
+	} catch (error) {
+		console.log(error);
+		logger.error(error);
+	}
 }
 
 exports.memberJoin = memberJoin;
